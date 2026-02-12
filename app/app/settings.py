83514162a70472ad4917dadce52105b565debe4a
Nikhilcs36 <nikhilcs36@gmail.com>
2026-02-13 03:13:08 +0530

    added a .env.example file and adding fallback values in settings.py.

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ykhkbxvzrbn%kc2kl^t1y%f5rp17w$0wlf1%$28!-x3#83qn4d')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
